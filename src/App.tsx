4c14cd5a5f62f8f4979179a43c28caf25b882cba
Yinjtcflm <1529792769@163.com>
2022-12-12 23:16:43 +0800

    创建Layout组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
+import Layout from 'components/Layout';
 import { HashRouter as Router, Redirect ,Route,Switch} from 'react-router-dom';
 import styled from 'styled-components';
 import Nav from './components/Nav';
 
-const Wrapper=styled.div`
-  height:100vh;
-  display:flex;
-  flex-direction:column;
-`;
 
-const Main=styled.div`
-  flex-grow:1;
-  overflow:auto;
-  
-`
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
           <Switch>
           <Route path="/tags">
             <Tags />
@@ -35,22 +23,30 @@ function App() {
             <NoMatch />
           </Route>
           </Switch>
-          </Main>
-          <Nav />
-          
-      </Wrapper>
     </Router>
   );
 }
 
 function Statistics(){
-  return <h2>统计页面</h2>
+  return (
+    <Layout>
+    <h2>统计页面</h2>
+    </Layout>
+  )
 }
 function Tags(){
-  return <h2>标签页面</h2>
+  return (
+    <Layout>
+    <h2>标签页面</h2>
+    </Layout>
+  )
 }
 function Money(){
-  return <h2>记账页面</h2>
+  return (
+    <Layout>
+    <h2>记账页面</h2>
+    </Layout>
+  )
 }
 
 function NoMatch(){

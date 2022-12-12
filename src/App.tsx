087a587c24e7c1f59259911efd8b0f4fa113bff2
Yinjtcflm <1529792769@163.com>
2022-12-12 22:24:21 +0800

    完成icon布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Redirect ,Route,Switch,Link} from 'react-router-dom';
+import { HashRouter as Router, Redirect ,Route,Switch} from 'react-router-dom';
 import styled from 'styled-components';
 import Nav from './components/Nav';
 

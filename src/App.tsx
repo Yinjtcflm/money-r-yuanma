7d27c9bf4aea2e6a0bd7acfda0a27b787810052f
Yinjtcflm <1529792769@163.com>
2022-12-13 22:17:04 +0800

    记账页面tags UI完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,37 @@
-import { HashRouter as Router, Redirect ,Route,Switch} from 'react-router-dom';
-import Money from 'views/Money';
-import NoMatch from 'views/NoMatch';
-import Statistics from 'views/Statistics';
-import Tags from 'views/Tags';
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Money from "views/Money";
+import NoMatch from "views/NoMatch";
+import Statistics from "views/Statistics";
+import Tags from "views/Tags";
+import styled from "styled-components";
 
-
+const AppWrapper = styled.div`
+  color: #333;
+`;
 
 function App() {
   return (
     <Router>
-          <Switch>
-          <Route path="/tags">
-            <Tags />
-          </Route>
-          <Route path="/money">
-            <Money />
-          </Route>
-          <Route path="/statistics">
-            <Statistics />
-          </Route>
-          <Redirect exact from='/' to='/money'></Redirect>
-          <Route path="*">
-            <NoMatch />
-          </Route>
-          </Switch>
+      <Switch>
+        <Route path="/tags">
+          <Tags />
+        </Route>
+        <Route path="/money">
+          <Money />
+        </Route>
+        <Route path="/statistics">
+          <Statistics />
+        </Route>
+        <Redirect exact from="/" to="/money"></Redirect>
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
     </Router>
   );
 }

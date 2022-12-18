1c597b76b85ef890964bb8bd12968730db25a793
Yinjtcflm <1529792769@163.com>
2022-12-18 23:28:12 +0800

    抽离Button.tsx

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,11 +1,31 @@
 import { useParams } from "react-router-dom";
 import { useTags } from "useTags";
+import Layout from "components/Layout";
+import Icon from "components/Icon";
+import { Button } from "components/Button";
+
 type Params = { id: string };
 const Tag: React.FC = (props) => {
   const { findTag } = useTags();
   let { id } = useParams<Params>();
   const tag = findTag(parseInt(id));
-  return <div>{tag.name}</div>;
+  return (
+    <Layout>
+      <header>
+        <Icon name="left" />
+        <span>编辑标签</span>
+      </header>
+      <div>
+        <label>
+          <span>标签名</span>
+          <input type="text" placeholder="标签名" />
+        </label>
+      </div>
+      <div>
+        <Button>删除标签</Button>
+      </div>
+    </Layout>
+  );
 };
 
 export { Tag };

dedae850a0f8ab466a0dee8e7f0a8f3861dff8d2
Yinjtcflm <1529792769@163.com>
2022-12-18 23:16:49 +0800

    封装findTag到useTags里

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,5 +1,11 @@
-const Tag: React.FC = () => {
-  return <div>hello</div>;
+import { useParams } from "react-router-dom";
+import { useTags } from "useTags";
+type Params = { id: string };
+const Tag: React.FC = (props) => {
+  const { findTag } = useTags();
+  let { id } = useParams<Params>();
+  const tag = findTag(parseInt(id));
+  return <div>{tag.name}</div>;
 };
 
 export { Tag };

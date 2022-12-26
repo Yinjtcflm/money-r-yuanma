8f895ead48fe2af7347b3ee619724c399cf7e3b6
Yinjtcflm <1529792769@163.com>
2022-12-26 22:39:39 +0800

    展示所有记录

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -19,6 +19,10 @@ const defaultFormData = {
   category: "-" as Category,
   amount: 0,
 };
+
+const CategoryWrapper = styled.div`
+  background: #c4c4c4;
+`;
 function Money() {
   const [selected, setSelected] = useState(defaultFormData);
   const { records, addRecord } = useRecords();
@@ -34,6 +38,7 @@ function Money() {
       setSelected(defaultFormData);
     }
   };
+
   return (
     <MyLayout>
       <TagsSection
@@ -44,10 +49,12 @@ function Money() {
         value={selected.note}
         onChange={(note) => onChange({ note })}
       />
-      <CategorySection
-        value={selected.category}
-        onChange={(category) => onChange({ category })}
-      />
+      <CategoryWrapper>
+        <CategorySection
+          value={selected.category}
+          onChange={(category) => onChange({ category })}
+        />
+      </CategoryWrapper>
       <NumberPadSection
         value={selected.amount}
         onChange={(amount) => onChange({ amount })}

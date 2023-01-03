77c828feeb8a924cfc8c1f0433c34872fe226c42
Yinjtcflm <1529792769@163.com>
2023-01-03 22:21:40 +0800

    小优化

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@ cd build &&
 git init &&
 git add . &&
 git commit -m 'update' &&
-git remote add origin git@github.com:Yinjtcflm/money-r.git &&
+#git remote add origin git@github.com:Yinjtcflm/money-r.git &&
+git remote add origin git@gitee.com:Yinjtcflm/money-r.git &&
 git push -u origin master -f 
 cd -
